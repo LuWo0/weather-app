3f720406f894afd707e0fea676b0b7c306c869cd
LuWo0 <loviedo46@yahoo.com>
2022-09-01 23:55:47 -0700

    refactored some js, html, and added more styling

--- src/index.js
+++ src/index.js
@@ -1,43 +1,39 @@
-const API_KEY = "1c13606af81639ce7dce09b093fd9ccd";
+const API_KEY = "1c13606af81639ce7dce09b093fd9ccd"; //Probably should hide this but whatever
+const weatherData = document.querySelector("[data-weather]");
+const searchQuery = document.getElementById("searchbar");
+
+// object for storing info of a particular place
 const place = {
     cityName: "",
     stateId: "",
     country: ""
 }
 
-
-const userDiv = document.querySelector("[data-user]");
-const searchQuery = document.getElementById("searchbar");
-
-
+// Fetch data from API (error handling will happen at a higher level)
 const getData = async (place) => {
-    
     const user = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${place.cityName},${place.stateId},${place.country}&appid=${API_KEY}&units=imperial`,
         {mode: 'cors'}
         );
     const userData = await user.json();
-    return userData;
-    
+    return userData;    
 }
 
 searchQuery.addEventListener("search", () => {
-    let input = searchQuery.value.split(",");
-    place.cityName = input[0];
-    place.stateId = input[1];
-    place.country = input[2];
+    const input = searchQuery.value.split(",");
+    [place.cityName, place.stateId, place.country] = [...input];
     console.log(place);
-    getData(place).then((data) => {
-        console.log(data);
-        userDiv.textContent = `City: ${data.name} current temperature (F): ${data.main.temp}`;    
-    }).catch(err => {userDiv.textContent = "No search results found."});
-    
+    getData(place)
+        .then((data) => {
+            console.log(data);
+            weatherData.textContent = `City: ${data.name} current temperature (F): ${data.main.temp}`;
+        })
+        .catch(err => {
+            weatherData.textContent = "No search results found."
+        });
 });
 
-// getData().then((data) => {
-//     userDiv.textContent = `City: ${data.name} current temperature (F): ${data.main.temp}`;    
-//     console.log(data);
-// });
+
 
 
 
